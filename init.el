126bc7aca7f8a59d788df3537fdbc513defcd13c
Carlos Sanchez Marin <casamatechs@gmail.com>
2016-02-11 09:00:42 +0100

    New config

--- init.el
+++ init.el
@@ -1,2 +1,14 @@
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-(load-theme 'misterioso)
+;;(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+;;(load-theme 'misterioso)
+
+
+
+(desktop-save-mode 1)
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   '("melpa" . "http://melpa.org/packages/")
+   t)
+  (package-initialize))
+(require 'darcula-theme)
