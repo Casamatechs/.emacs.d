58ac64dd17c49c663973a878690d02e0e8d10610
Carlos Sanchez Marin <casamatechs@gmail.com>
2016-07-29 11:28:20 +0200

    Closing old laptop

--- init.el
+++ init.el
@@ -22,3 +22,11 @@
 ;; add the emacs-eclim source
 ;(require 'ac-emacs-eclim-source)
 ;(ac-emacs-eclim-config)
+
+(require 'ox-latex)
+(unless (boundp 'org-latex-classes)
+  (setq org-latex-classes nil))
+(add-to-list 'org-latex-classes
+             '("article"
+               "\\documentclass{article}"
+               ("\\section{%s}" . "\\section*{%s}")))
