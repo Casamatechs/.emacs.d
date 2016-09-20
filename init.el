050f0b17f12335dcc627b4035794b1e5121d164b
Carlos Sanchez Marin <casamatechs@gmail.com>
2016-09-20 16:53:15 +0200

    Added scala-mode

--- init.el
+++ init.el
@@ -66,3 +66,17 @@
 ;;; matlab-settings.el ends here
 
 (setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (scala-mode use-package matlab-mode emacs-eclim darcula-theme auto-complete))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
