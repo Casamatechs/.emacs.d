56a2aec1ebc95922cbfea0bda694d255d0dbce26
Carlos Sanchez Marin <casamatechs@gmail.com>
2018-04-03 12:44:55 +0200

    First commit

---
+++ init.el
@@ -0,0 +1,127 @@
+;; No bars pls
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(add-to-list 'exec-path "/usr/local/bin")
+(global-set-key (kbd "M-1") "|")
+(global-set-key (kbd "M-2") "@")
+(global-set-key (kbd "M-3") "#")
+(global-set-key (kbd "M-º") "\\")
+(global-set-key (kbd "M-ç") "}")
+(global-set-key (kbd "M-+") "]")
+
+(desktop-save-mode 1)
+
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   '("melpa" . "http://melpa.org/packages/") t)
+  (add-to-list
+   'package-archives
+   '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
+  (package-initialize))
+(unless (featurep 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package)
+  )
+
+(use-package powerline
+  :ensure t
+  :config (powerline-default-theme))
+
+;; Rainbow delimeters
+(use-package rainbow-delimiters
+  :ensure t
+  :init
+  (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+  )
+
+;; Rainbow mode
+(use-package rainbow-mode
+  :ensure t
+  :mode "\\.css\\'"
+  )
+
+;; Yasnippets
+(use-package yasnippet
+  :ensure t
+  :init (yas-global-mode 1)
+  :config
+  (define-key yas-minor-mode-map (kbd "<tab>") nil)
+  (define-key yas-minor-mode-map (kbd "TAB") nil)
+  (define-key yas-minor-mode-map (kbd "<C-tab>") 'yas-expand)
+  )
+
+;; SmartParents
+(use-package smartparens
+  :ensure t
+  :init (smartparens-global-mode t))
+
+(use-package alchemist
+  :ensure t)
+
+;; elixir-mode hook
+(add-hook 'elixir-mode-hook
+          (lambda () (add-hook 'before-save-hook 'mix-format-before-save)))
+
+;; yasnippets
+(use-package elixir-yasnippets
+  :ensure t)
+
+;; darcula-theme
+(use-package darcula-theme
+  :ensure t
+  :config (load-theme 'darcula t)
+  ;; your preferred main font face here
+  )
+
+;;linum-relative
+(use-package linum-relative
+  :ensure t)
+
+;; Auto complete
+(use-package auto-complete
+  :ensure t
+  :config (ac-config-default))
+
+;; Flycheck
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("3d5720f488f2ed54dd4e40e9252da2912110948366a16aef503f3e9e7dfe4915" default)))
+ '(ensime-sem-high-faces
+   (quote
+    ((var :foreground "#9876aa" :underline
+	  (:style wave :color "yellow"))
+     (val :foreground "#9876aa")
+     (varField :slant italic)
+     (valField :foreground "#9876aa" :slant italic)
+     (functionCall :foreground "#a9b7c6")
+     (implicitConversion :underline
+			 (:color "#808080"))
+     (implicitParams :underline
+		     (:color "#808080"))
+     (operator :foreground "#cc7832")
+     (param :foreground "#a9b7c6")
+     (class :foreground "#4e807d")
+     (trait :foreground "#4e807d" :slant italic)
+     (object :foreground "#6897bb" :slant italic)
+     (package :foreground "#cc7832")
+     (deprecated :strike-through "#a9b7c6"))))
+ '(package-selected-packages (quote (use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
